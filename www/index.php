e38eb2011e72162b3748074a435b0c09bd8e3937
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2023-01-10 14:13:23 +0100

    raphael init

--- www/index.php
+++ www/index.php
@@ -13,7 +13,7 @@ if (isset($_GET['page'])) {
 include_once __DIR__ . "/../src/templates/pages/$page.php";
 include_once __DIR__ . "/../src/templates/template.php";
 // include_once __DIR__ . "/../src/templates/partials/head.php";
-// echo $page_content;
+// echo $page_conten t;
 // include_once __DIR__ . "/../src/templates/partials/footer.php";
 //index
 
