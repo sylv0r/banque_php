1b9aee97a97b92bd5db11143e8d70a4df6e1cda8
fabien <fabien.arnal@edu.esiee-it.fr>
2023-01-10 16:49:25 +0100

    header footer

--- src/templates/template.php
+++ src/templates/template.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">		<link rel="stylesheet" href="/css/header.css">
 		<link rel="stylesheet" href="/css/header.css">
 		<link rel="stylesheet" href="/css/footer.css">
 		<title><?= $page_title; ?></title>
@@ -19,11 +19,11 @@
         </div>
 
 
-	<?= $page_content ;?>
-	<?= $page_scripts ;?>
+		<?= $page_content ;?>
+		<?= $page_scripts ;?>
 
-	<?php 
-	require_once __DIR__ . '/partials/footer.php';
-	?>
+		<?php 
+		require_once __DIR__ . '/partials/footer.php';
+		?>
 	</body>
 </html>
