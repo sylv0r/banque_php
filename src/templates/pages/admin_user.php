5673111ea66b068eeff37a5398bacb631ca16762
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2023-01-12 16:59:32 +0100

    correction

--- src/templates/pages/admin_user.php
+++ src/templates/pages/admin_user.php
@@ -13,46 +13,50 @@ show_error();
 
 <h1>Manager user</h1>
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Email</th>
-      <th scope="col">Role</th>
-      <th scope="col">Created_At</th>
-      <th scope="col">Last_IP</th>
-      <th scope="col"> </th>
-    </tr>
-  </thead>
-  <tbody><?php
-    foreach ($requete as $row){
-        ?>
-            <tr>
-                <td><?=$row->id?></td>
-                <td><?=$row->email?></td>
-                <td>
-                <form action="/actions/update_admin_user.php" method="post" >
-                    <select name="role">
-                        <option>-- attribuez un rôle --</option>
-                        <option value="10">vérifié</option>
-                        <option value="0">banni</option>
-                    </select>
-                </td>
-                <td><?=$row->created_at?></td>
-                <td><?=$row->last_ip?></td>
-                <td>
-                <?php if ($role == 200) { ?>
-                    <input type="hidden" name="user_id" value="<?= $row->id ?>">
-                    <button type="submit" class="btn btn-success btn-sm">Valider</button>
-                <?php } else echo "Vous n'avez pas les droits"; ?>
-                </form>
-            </td>
-            </tr>
-        <?php
-     } ?>
-    
-  </tbody>
-</table>
-
+<?php if (count($requete) > 0) { ?>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Email</th>
+        <th scope="col">Role</th>
+        <th scope="col">Created_At</th>
+        <th scope="col">Last_IP</th>
+        <th scope="col"> </th>
+      </tr>
+    </thead>
+    <tbody><?php
+    foreach ($requete as $row) {
+      ?>
+              <tr>
+                  <td><?= $row->id ?></td>
+                  <td><?= $row->email ?></td>
+                  <td>
+                  <form action="/actions/update_admin_user.php" method="post" >
+                      <select name="role">
+                          <option>-- attribuez un rôle --</option>
+                          <option value="10">vérifié</option>
+                          <option value="0">banni</option>
+                      </select>
+                  </td>
+                  <td><?= $row->created_at ?></td>
+                  <td><?= $row->last_ip ?></td>
+                  <td>
+                  <?php if ($role == 200) { ?>
+                      <input type="hidden" name="user_id" value="<?= $row->id ?>">
+                      <button type="submit" class="btn btn-success btn-sm">Valider</button>
+                  <?php } else
+                    echo "Vous n'avez pas les droits"; ?>
+                  </form>
+              </td>
+              </tr>
+          <?php
+    } ?>
+      
+    </tbody>
+  </table>
 <?php
+} else {
+  echo "Aucune demande en attente";
+}
 $page_content = ob_get_clean();
--- src/templates/partials/menu.php
+++ src/templates/partials/menu.php
@@ -1,14 +1,17 @@
 <ul>
     <li><a href="/?page=home">Home</a></li>
-    <li><a href="/?page=contact">contact</a></li>
+    <li><a href="/?page=contact">Contact</a></li>
 <?php if ($user === false){ ?>
-    <li><a href="/?page=singup">Singup</a></li>
+    <li><a href="/?page=singup">Signup</a></li>
     <li><a href="/?page=login">Login</a></li>
 <?php } else { ?>
     <li><?= $user->email ?></li>
     <li><a href="/?page=bank_account">Bank Account</a></li>
     <li><a href="/?page=transfer">Transfer</a></li>
+    <?php if ($user->role >= 200) { ?>
+    <li><a href="/?page=admin_contact">Admin contact</a></li>
+    <?php if ($user->role >= 1000) { ?>
+    <li><a href="/?page=admin_user">Manage accounts</a></li>
+    <?php }}} ?>
     <li><a href="/actions/logout.php">Logout</a></li>
-    <li><a href="/?page=admin_contact">admin_contact</a></li>
-    <li><a href="/?page=admin_user">admin_user</a></li>
-</ul> <?php } 
+</ul> 
