d417b5f91d9a1da65fd1d3e8e1b7b173d4b995a1
sylv0r <sylvian.vidal95@gmail.com>
2023-01-13 13:30:52 +0100

    finito

--- src/templates/partials/menu.php
+++ src/templates/partials/menu.php
@@ -5,31 +5,13 @@
     <li class="nav-item"><a href="/?page=singup" class="nav-link">Singup</a></li>
     <li class="nav-item"><a href="/?page=login" class="nav-link">Login</a></li>
 <?php } else { ?>
-    <li class="nav-item"><a href="/?page=contact" class="nav-link">contact</a></li>
+    <li class="nav-item"><a href="/?page=contact" class="nav-link">Contact</a></li>
     <li class="nav-item"><a href="/?page=bank_account" class="nav-link">Bank Account</a></li>
     <li class="nav-item"><a href="/?page=transfer" class="nav-link">Transfer</a></li>
-    <li class="nav-item"><a href="/?page=my_transaction" class="nav-link">mes transaction</a></li>
-
+    <li class="nav-item"><a href="/?page=my_transaction" class="nav-link">Mes transactions</a></li>
 
     <li class="nav-item"><a href="/?page=depot" class="nav-link">Dépot</a></li>
     <li class="nav-item"><a href="/?page=retrait" class="nav-link">Retrait</a></li>
-    <?php if ($user->role >= 200) { ?>
-    <li class="nav-item"><a href="/?page=admin_contact" class="nav-link">admin_contact</a></li>
-    <li class="nav-item"><a href="/?page=admin_user" class="nav-link">admin_user</a></li>
-    <li class="nav-item"><a href="/?page=admin_depot" class="nav-link">admin_depot</a></li>
-    <li class="nav-item"><a href="/?page=admin_retrait" class="nav-link">admin_retrait</a></li>
-
-    <?php if ($user->role >= 1000) { ?>
-        <li class="nav-item"><a href="/?page=transaction" class="nav-link">transaction</a></li>
-        <li class="nav-item"><a class="nav-link" href="/?page=admin">Admin panel</a></li>
-    <?php }}} ?>
-
-
-
-
-    <li class="nav-item"><a href="/actions/logout.php" class="nav-link">logout</a></li>
-    
-
     <?php if ($user->role >= 200) { ?>
     <li class="nav-item"><a href="/?page=admin_contact" class="nav-link">Admin contact</a></li>
     <li class="nav-item"><a href="/?page=admin_user" class="nav-link">Admin signup</a></li>
