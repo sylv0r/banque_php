027bf1224ca95523c7742c551b40991c09803b3f
fabien <fabien.arnal@edu.esiee-it.fr>
2023-01-13 10:17:12 +0100

    modif menu

--- src/templates/partials/menu.php
+++ src/templates/partials/menu.php
@@ -1,13 +1,13 @@
-<ul>
-    <li><a href="/?page=home">Home</a></li>
-    <li><a href="/?page=contact">contact</a></li>
+<ul class="nav nav-tabs mb-4">
+    <li class="nav-item"><a href="/?page=home" class="nav-link">Home</a></li>
+    <li class="nav-item"><a href="/?page=contact" class="nav-link">contact</a></li>
 <?php if ($user === false){ ?>
-    <li><a href="/?page=singup">Singup</a></li>
-    <li><a href="/?page=login">Login</a></li>
+    <li class="nav-item"><a href="/?page=singup" class="nav-link">Singup</a></li>
+    <li class="nav-item"><a href="/?page=login" class="nav-link">Login</a></li>
 <?php } else { ?>
-    <li><a href="/?page=bank_account">Bank Account</a></li>
-    <li><a href="/actions/logout.php">Logout</a></li>
+    <li class="nav-item"><a href="/?page=bank_account" class="nav-link">Bank Account</a></li>
+    <li class="nav-item"><a href="/actions/logout.php" class="nav-link">Logout</a></li>
 <?php } ?>
-    <li><a href="/?page=admin_contact">admin_contact</a></li>
-    <li><a href="/?page=admin_user">admin_user</a></li>
+    <li class="nav-item"><a href="/?page=admin_contact" class="nav-link">admin_contact</a></li>
+    <li class="nav-item"><a href="/?page=admin_user" class="nav-link">admin_user</a></li>
 </ul>
